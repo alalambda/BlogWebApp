382c7605d655e5501c602236b393e9ac656c1af9
Anna Timoskina <anna.timoskina@accenture.com>
2019-03-19 11:18:38 +0200

    added individual user auth

--- BlogWebApplication/Properties/AssemblyInfo.cs
+++ BlogWebApplication/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("11b9da0a-d3b1-423a-ad80-35ec9f202a19")]
+[assembly: Guid("d7cd8a4d-5a4e-4d76-90f7-dc76f474316d")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ BlogWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogWebApplication.Startup))]
+namespace BlogWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
